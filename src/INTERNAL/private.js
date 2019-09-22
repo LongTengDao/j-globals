2e1e077589f905095d418ad2a58d40fd37b54f8f
龙腾道 <LongTengDao@LongTengDao.com>
2019-09-22 18:41:16 +0800

    v13.0.0

--- src/INTERNAL/private.js
+++ src/INTERNAL/private.js
@@ -1,28 +1,32 @@
 import create from '.Object.create';
 import defineProperty from '.Object.defineProperty';
-import RETURN from '.return';
 import NULL from '.null.prototype';
+import undefined from '.undefined';
 export default (
 	/*! j-globals: private (internal) */
-	typeof WeakMap==='function'
-		? /*#__PURE__*/ function () {
-			var Weak = WeakMap;
-			var GET = create(NULL);
-			GET.value = Weak.prototype.get;
-			var SET = create(NULL);
-			SET.value = Weak.prototype.set;
-			function add (weak, THIS) {
-				var _THIS = create(NULL);
-				weak.set(THIS, _THIS);
-				return _THIS;
-			}
-			return function Private () {
-				var weak = /*#__PURE__*/ defineProperty(/*#__PURE__*/ defineProperty(/*#__PURE__*/ new Weak, 'get', GET), 'set', SET);
-				return function _ (THIS) {
-					return /*#__PURE__*/ weak.get(THIS) || /*#__PURE__*/ add(weak, THIS);
-				};
+	/*#__PURE__*/ function (WeakMap) {
+		var GET = create(NULL);
+		GET.value = WeakMap.prototype.get;
+		var SET = create(NULL);
+		SET.value = WeakMap.prototype.set;
+		function set (weak, THIS, _THIS) {
+			weak.set(THIS, _THIS);
+			return _THIS;
+		}
+		function Null () { return create(NULL); }
+		return function Private (PRIVATE) {
+			var weak = /*#__PURE__*/defineProperty(/*#__PURE__*/defineProperty(/*#__PURE__*/new WeakMap, 'get', GET), 'set', SET);
+			var _This = PRIVATE===undefined
+				? Null
+				: typeof PRIVATE==='function'
+					? 'prototype' in PRIVATE
+						? function (THIS) { return new PRIVATE(THIS); }
+						: function (THIS) { return PRIVATE(THIS); }
+					: function () { return create(PRIVATE); };
+			return function _ (THIS) {
+				return /*#__PURE__*/weak.get(THIS) || /*#__PURE__*/set(weak, THIS, /*#__PURE__*/_This(THIS));
 			};
-		}()
-		: function Private () { return RETURN; }
+		};
+	}(WeakMap)
 	/*¡ j-globals: private (internal) */
 );
