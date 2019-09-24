83b704b22a2f6300f93700b0d05fb231e6757d4d
龙腾道 <LongTengDao@LongTengDao.com>
2019-09-24 14:54:27 +0800

    v13.1.2

--- dist/NPM/index.js
+++ dist/NPM/index.js
@@ -1,6 +1,6 @@
 ﻿'use strict';
 
-const version = '13.1.1';
+const version = '13.1.2';
 
 const assign = Object.assign;
 
@@ -1052,10 +1052,10 @@ function toTSD (all                              , { bom = false, tab = '\t', eo
 						break;
 					case 'private':
 						tsd += trim`Private;${eol}
-							${tab}function Private<_ extends (this :void, instance :any) => object> (Private? :ReturnType<_> | { (this :void, $ :object) :ReturnType<_> } | { (this :void) :ReturnType<_> } | { new ($ :object) :ReturnType<_> } | { new () :ReturnType<_> }) :_;${eol}
-							${tab}function Private<Public extends object, Private extends object> (PRIVATE? :Private) :(this :void, instance :Public) => Private;${eol}
+							${tab}function Private<_ extends (this :void, instance :any) => object> (Private? :{ new ($ :object) :ReturnType<_> } | { new () :ReturnType<_> } | { (this :void, $ :object) :ReturnType<_> } | { (this :void) :ReturnType<_> } | ReturnType<_>) :_;${eol}
+							${tab}function Private<Public extends object, PrivateConstructor extends { new ($ :Public) :object } | { new () :object }> (Private :PrivateConstructor) :(this :void, instance :Public) => InstanceType<PrivateConstructor>;${eol}
 							${tab}function Private<Public extends object, PrivateApplier extends { ($ :Public) :object } | { () :object }> (Private :PrivateApplier) :(this :void, instance :Public) => ReturnType<PrivateApplier>;${eol}
-							${tab}function Private<Public extends object, PrivateConstructor extends { new ($ :Public) :object } | { new () :object }> (Private :PrivateConstructor) :(this :void, instance :Public) => InstanceType<PrivateConstructor>;${eol}`;
+							${tab}function Private<Public extends object, Private extends object> (PRIVATE? :Private) :(this :void, instance :Public) => Private;${eol}`;
 						break;
 					case 'for.of':
 						tsd += `of;${eol}${tab}function of<V extends any, T extends any> (arrayLike_iterable :Iterable<V>, callbackfn :(this :T, value :V) => void | boolean, thisArg? :T) :void;${eol}`;
